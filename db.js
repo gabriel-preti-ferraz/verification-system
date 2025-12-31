cb4d7b83c3dbaee4dd970ce82042b4eafa85ce46
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2025-12-31 16:01:10 -0300

    feat: database

---
+++ db.js
@@ -0,0 +1,29 @@
+import Database from "better-sqlite3"
+const db = new Database("database.db")
+
+db.pragma("journal_mode = WAL")
+db.pragma("foreign_keys = ON")
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        email TEXT UNIQUE NOT NULL
+    )
+    `).run()
+
+db.prepare(`
+    CREATE TABLE IF NOT EXISTS projects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+
+    created_at INTEGER NOT NULL,
+    expires_at INTEGER NOT NULL,
+    status TEXT NOT NULL,
+
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+    )
+    `).run()
+
+export default db
