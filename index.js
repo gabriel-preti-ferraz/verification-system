1b9f067e2037fecebff1ce751c204a26c8c41b06
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2025-12-31 16:42:57 -0300

    feat: license check route

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import dotenv from "dotenv"
 dotenv.config({ path: "./.env" })
 import verifyToken from "./middlewares/verifyToken.js"
+import checkLicense from "./licenseController.js"
 import db from "./database/db.js"
 
 const app = express()
@@ -13,4 +14,15 @@ app.listen(process.env.API_PORT, () => console.log(`Server is running on PORT ${
 
 app.get("/license/check/:projectId", verifyToken, async (req, res) => {
     const projectId = req.params.projectId
+
+    try {
+        const license = await checkLicense(projectId)
+
+        if (license.status === 404) return res.status(404).json(license)
+        
+        res.json(license)
+    } catch (err) {
+        console.error(err)
+        res.status(500).json({status: "error", message: "Internal server error"})
+    }
 })
