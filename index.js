b216765bd3fffba1dd08c4686bb681c270a5863c
Gabriel Preti Ferraz <125829214+gabriel-preti-ferraz@users.noreply.github.com>
2026-01-01 09:46:04 -0300

    feat: create users and projects route

--- index.js
+++ index.js
@@ -4,13 +4,49 @@ import dotenv from "dotenv"
 dotenv.config({ path: "./.env" })
 import verifyToken from "./middlewares/verifyToken.js"
 import checkLicense from "./licenseController.js"
-import db from "./database/db.js"
+import client from "./database/db.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 app.listen(process.env.API_PORT, () => console.log(`Server is running on PORT ${process.env.API_PORT}`))
+
+app.post("/users/create", async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const result = await client.query(
+            "INSERT INTO users (username, email, password) VALUES($1, $2, $3)",
+            [username, email, hashedPassword]
+        )
+        res.status(201).json(result.rows[0])
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Server error")
+    }
+})
+
+app.post("/projects/create", async (req, res) => {
+    try {
+        const { name, expires_at, user_id } = req.body
+
+        const result = await client.query(
+            "INSERT INTO projects (name, expires_at, user_id) VALUES ($1, $2, $3)",
+            [name, expires_at, user_id]
+        )
+
+        const token = jwt.sign({userId: user_id}, process.env.SECRET_KEY)
+
+        console.log(result, token)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send("Server Error")
+    }
+})
 
 app.get("/license/check/:projectId", verifyToken, async (req, res) => {
     const projectId = req.params.projectId
